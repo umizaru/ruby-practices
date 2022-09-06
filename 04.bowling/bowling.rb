84a4d820c6f3d0305d4e9a4a30f27698c43371e2
umizaru <y.wakita1T@gmail.com>
2022-09-06 23:47:51 +0700

    before final check

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,17 +1,16 @@
 require 'optparse'
-require 'debug'
-
 numbers = ARGV[0].gsub("X","10").split(",").map(&:to_i)
 score = 0
-numbers.each_with_index do |item, i|
-    if numbers[i] == 10
-        score += numbers[i]+numbers[i+1]+numbers[i+2]
+10.times {
+    if numbers[0] == 10
+        score += numbers[0]+numbers[1]+numbers[2]
         numbers.shift(1)
-    elsif numbers[i] + numbers[i+1] == 10
-        score += numbers[i]+numbers[i+1]+numbers[i+2]
+    elsif numbers[0] + numbers[1] == 10
+        score += numbers[0]+numbers[1]+numbers[2]
         numbers.shift(2)
-    elsif numbers[i] + numbers[i+1] != 10
-        score += numbers[i]+numbers[i+1]+numbers[i+2]
+    elsif numbers[0] + numbers[1] != 10
+        score += numbers[0]+numbers[1]
         numbers.shift(2)
     end
-end
+}
+puts score
