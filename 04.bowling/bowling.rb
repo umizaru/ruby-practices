70444939df6ecc0c6a41aa9a150496c055ebd753
umizaru <y.wakita1T@gmail.com>
2022-09-07 00:20:16 +0700

    after rubocop-OK

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,16 +1,16 @@
 require 'optparse'
-numbers = ARGV[0].gsub("X","10").split(",").map(&:to_i)
+numbers = ARGV[0].gsub('X', '10').split(',').map(&:to_i)
 score = 0
-10.times {
-    if numbers[0] == 10
-        score += numbers[0]+numbers[1]+numbers[2]
-        numbers.shift(1)
-    elsif numbers[0] + numbers[1] == 10
-        score += numbers[0]+numbers[1]+numbers[2]
-        numbers.shift(2)
-    elsif numbers[0] + numbers[1] != 10
-        score += numbers[0]+numbers[1]
-        numbers.shift(2)
-    end
-}
+10.times do
+  if numbers[0] == 10
+    score += numbers[0] + numbers[1] + numbers[2]
+    numbers.shift(1)
+  elsif numbers[0] + numbers[1] == 10
+    score += numbers[0] + numbers[1] + numbers[2]
+    numbers.shift(2)
+  elsif numbers[0] + numbers[1] != 10
+    score += numbers[0] + numbers[1]
+    numbers.shift(2)
+  end
+end
 puts score
