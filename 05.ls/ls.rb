3b1ebe1ea84cd4631b62c76e662d94e23f9ca081
umizaru <y.wakita1T@gmail.com>
2022-09-23 01:14:06 +0900

    after rubocop

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 require 'debug'
 
-current_dir_files = Dir.glob("*") #全ファイル配列
-rows = 3 #列数
-number_of_files = current_dir_files.size #ファイル数
-columns = (number_of_files.to_f / rows).ceil #行数
+current_dir_files = Dir.glob('*') # 全ファイル配列
+rows = 3 # 列数
+number_of_files = current_dir_files.size # ファイル数
+columns = (number_of_files.to_f / rows).ceil # 行数
 
-i = 0
 display_array = []
 
 columns.times do |i|
-  display_array << current_dir_files.values_at(i,i+columns,i+columns*2)
+  display_array << current_dir_files.values_at(i, i + columns, i + columns * 2)
 end
 
 display_array.map do |n|
- puts n.join(' ')
+  puts n.join(' ')
 end
