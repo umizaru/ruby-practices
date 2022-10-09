f8e1e799fbc817ca69a4839537af19bd2aced9c1
umizaru <y.wakita1T@gmail.com>
2022-10-09 20:34:40 +0900

    first commit

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -15,7 +15,7 @@ current_dir_files =
   end
 
 number_of_files = current_dir_files.size
-data_of_rows = (number_of_files.to_f / number_of_columns).ceil
-data_of_rows.times do |i|
-  puts current_dir_files.values_at(i, i + data_of_rows, i + data_of_rows * 2).join(' ')
+rows = (number_of_files.to_f / number_of_columns).ceil
+rows.times do |i|
+  puts current_dir_files.values_at(i, i + rows, i + rows * 2).join(' ')
 end
