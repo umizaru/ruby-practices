5faf6115bebd09929b0fcad26bf9657be05ac356
umizaru <y.wakita1T@gmail.com>
2022-10-13 21:53:17 +0900

    after rubocop

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,21 +5,16 @@ require 'optparse'
 NUMBER_OF_COLUMNS = 3
 BETWEEN_COLUMNS = 4
 
-options = ARGV.getopts('a')
+ARGV.getopts('r')
+current_dir_files = Dir.glob('*')
+reverse_current_dir_files = current_dir_files.reverse
 
-current_dir_files =
-  if options['a']
-    Dir.glob(['.*', '*'])
-  else
-    Dir.glob('*')
-  end
-
-number_of_files = current_dir_files.size
+number_of_files = reverse_current_dir_files.size
 rows = (number_of_files.to_f / NUMBER_OF_COLUMNS).ceil
 
-width = current_dir_files.map(&:size).max + BETWEEN_COLUMNS
-current_dir_files = current_dir_files.map { |x| x.ljust(width) }
+width = reverse_current_dir_files.map(&:size).max + BETWEEN_COLUMNS
+reverse_current_dir_files = reverse_current_dir_files.map { |x| x.ljust(width) }
 
 rows.times do |i|
-  puts current_dir_files.values_at(i, i + rows, i + rows * 2).join(' ')
+  puts reverse_current_dir_files.values_at(i, i + rows, i + rows * 2).join(' ')
 end
