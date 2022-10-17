ad2a24dcf26315b763b4007753171ae75eea76b5
umizaru <y.wakita1T@gmail.com>
2022-10-17 18:41:23 +0900

    first commit

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,59 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
+require 'time'
 
-NUMBER_OF_COLUMNS = 3
-BETWEEN_COLUMNS = 4
-
-options = ARGV.getopts('r')
+options = ARGV.getopts('l')
 current_dir_files = Dir.glob('*')
-current_dir_files = current_dir_files.reverse if options['r']
+current_dir_pass = Dir.pwd
 
-number_of_files = current_dir_files.size
-rows = (number_of_files.to_f / NUMBER_OF_COLUMNS).ceil
+stat = File.stat("01.dir")
 
-width = current_dir_files.map(&:size).max + BETWEEN_COLUMNS
-current_dir_files = current_dir_files.map { |x| x.ljust(width) }
+array = []
 
-rows.times do |i|
-  puts current_dir_files.values_at(i, i + rows, i + rows * 2).join(' ')
+# ファイルの種類
+if stat.ftype == "file"
+  array << "-"
+elsif stat.ftype == "directory"
+  array << "d"
+elsif stat.ftype == "link"
+  array << "l"
+end
+
+# パーミッション
+PERMISSION_PATTERN = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}.freeze
+permission_number = stat.mode.to_s(8)
+permission_number = permission_number.slice(3..5)
+permission_pattern = permission_number.gsub(/[01234567]/,PERMISSION_PATTERN)
+array <<  permission_pattern
+
+# ハードリンクの数
+array << stat.nlink
+
+# 所有者と所有グループ
+user = Etc.getpwuid(stat.uid).name
+group = Etc.getgrgid(stat.gid).name
+array << user
+array << group
+
+# ファイルサイズ
+array <<  stat.size
+
+# タイムスタンプ
+file_create_time = stat.atime
+month_of_file_create_time = file_create_time.strftime("%m")
+day_of_file_create_time = file_create_time.strftime("%d")
+minutes_of_file_create_time = file_create_time.strftime("%H:%M")
+
+array << month_of_file_create_time
+array << day_of_file_create_time
+array << minutes_of_file_create_time
+
+# ファイル名
+array << "01.dir"
+
+#　表示
+array.each do |x|
+  print x
 end
