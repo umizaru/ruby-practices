30a4b7d00cde377045874368c0a5351541479c5c
umizaru <y.wakita1T@gmail.com>
2022-10-21 22:29:03 +0900

    rubocop OK

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,36 +4,71 @@ require 'optparse'
 require 'etc'
 require 'time'
 
-PERMISSION_PATTERNS = {'0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx'}.freeze
+NUMBER_OF_COLUMNS = 3
+BETWEEN_COLUMNS = 4
+PERMISSION_PATTERNS = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
+NUMBER_OF_FILESTAT = 9
 
-options = ARGV.getopts('l')
+@options = ARGV.getopts('l')
+
+def entered_options
+  if @options['l']
+    output_option_l
+  else
+    output_option_none
+  end
+end
+
+# オプションなしの場合
+def output_option_none
+  current_dir_files = Dir.glob('*')
+
+  number_of_files = current_dir_files.size
+  rows = (number_of_files.to_f / NUMBER_OF_COLUMNS).ceil
+
+  width = current_dir_files.map(&:size).max + BETWEEN_COLUMNS
+  current_dir_files = current_dir_files.map { |x| x.ljust(width) }
+
+  rows.times do |i|
+    puts current_dir_files.values_at(i, i + rows, i + rows * 2).join(' ')
+  end
+end
+
 @current_dir_files = Dir.glob('*')
 @current_dir_pass = Dir.pwd
 
-@type_permission_array = Array.new
-@hardlink_array = Array.new
-@size_array = Array.new
-@name_array = Array.new
-@month_data_array = Array.new
-@day_data_array = Array.new
-@minutes_data_array = Array.new
-@user_array = Array.new
-@group_array = Array.new
+@block_array = []
+@type_permission_array = []
+@hardlink_array = []
+@size_array = []
+@name_array = []
+@month_data_array = []
+@day_data_array = []
+@minutes_data_array = []
+@user_array = []
+@group_array = []
+@all_data_array = []
+
+# ブロック数
+def block
+  @block_array << @file_stat.blocks
+end
 
 # ファイルのタイプとパーミッション
 def type_and_permission
-  if @file_stat.ftype == "file"
-    file_type = "-"
-  elsif @file_stat.ftype == "directory"
-    file_type = "d"
-  elsif @file_stat.ftype == "link"
-    file_type = "l"
+  case @file_stat.ftype
+  when 'file'
+    file_type = '-'
+  when 'directory'
+    file_type = 'd'
+  when 'link'
+    file_type = 'l'
   end
 
-permission_number = @file_stat.mode.to_s(8)
-permission_number = permission_number.split('').last(3).join('')
-file_permission = permission_number.gsub(/[01234567]/,PERMISSION_PATTERNS)
-@type_permission_array << file_type + file_permission
+  permission_number = @file_stat.mode.to_s(8)
+  permission_number = permission_number.split('').last(3).join('')
+  file_permission = permission_number.gsub(/[01234567]/, PERMISSION_PATTERNS)
+  @type_permission_array << file_type + file_permission
 end
 
 # ハードリンクの数
@@ -57,9 +92,9 @@ end
 # タイムスタンプ
 def timestamp
   file_create_time = @file_stat.atime
-  month_of_file_create_time = file_create_time.strftime("%m")
-  day_of_file_create_time = file_create_time.strftime("%d")
-  minutes_of_file_create_time = file_create_time.strftime("%H:%M")
+  month_of_file_create_time = file_create_time.strftime('%m')
+  day_of_file_create_time = file_create_time.strftime('%d')
+  minutes_of_file_create_time = file_create_time.strftime('%H:%M')
   @month_data_array << month_of_file_create_time
   @day_data_array << day_of_file_create_time
   @minutes_data_array << minutes_of_file_create_time
@@ -70,10 +105,11 @@ def name
   @name_array << @file_name
 end
 
+# 各要素で配列を作成
 @current_dir_files.each do |x|
   @file_stat = File.stat("#{@current_dir_pass}/#{x}")
-  @file_name = "#{x}"
-  blocks
+  @file_name = x.to_s
+  block
   type_and_permission
   hardlink
   owner
@@ -82,6 +118,7 @@ end
   name
 end
 
+# ファイル名の長さ
 width = @user_array.max.to_s.length
 @user_array = @user_array.map { |x| x.rjust(width) }
 
@@ -91,25 +128,42 @@ width = @group_array.max.to_s.length
 width = @size_array.max.to_s.length
 @size_array = @size_array.map { |x| x.to_s.rjust(width) }
 
-all_data_array = Array.new
+# すべての要素を含む配列を作成
 i = 0
-9.times {
-  all_data_array <<
-{
-  type_permission:@type_permission_array[i],
-  hardlink:@hardlink_array[i],
-  user:@user_array[i],
-  group:@group_array[i],
-  size:@size_array[i],
-  month_data:@month_data_array[i],
-  day_data:@day_data_array[i],
-  minutes_data:@minutes_data_array[i],
-  name:@name_array[i],
-}
-i += 1
-}
-
-all_data_array.each do |result|
-  print "#{result[:type_permission]}#{"  "}#{result[:hardlink]}#{" "}#{result[:user]}#{"  "}#{result[:group]}#{"  "}#{result[:size]}#{" "}#{result[:month_data]}#{" "}#{result[:day_data]}#{" "}#{result[:minutes_data]}#{" "}#{result[:name]}"
-  print "\n"
+NUMBER_OF_FILESTAT.times do
+  @all_data_array <<
+    {
+      type_permission: @type_permission_array[i],
+      hardlink: @hardlink_array[i],
+      user: @user_array[i],
+      group: @group_array[i],
+      size: @size_array[i],
+      month_data: @month_data_array[i],
+      day_data: @day_data_array[i],
+      minutes_data: @minutes_data_array[i],
+      name: @name_array[i]
+    }
+  i += 1
 end
+
+# 文字列として出力
+def output_option_l
+  print 'total '
+  print @block_array.inject(:+)
+  print "\n"
+
+  @all_data_array.each do |n|
+    print "#{n[:type_permission]}  "
+    print "#{n[:hardlink]} "
+    print "#{n[:user]}  "
+    print "#{n[:group]}  "
+    print "#{n[:size]} "
+    print "#{n[:month_data]} "
+    print "#{n[:day_data]} "
+    print "#{n[:minutes_data]} "
+    print (n[:name]).to_s
+    print "\n"
+  end
+end
+
+entered_options
