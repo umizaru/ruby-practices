cee31059c01cc3310a4c8fd49f5e0075fe3c5b32
umizaru <y.wakita1T@gmail.com>
2022-11-21 23:03:20 +0900

    すべての指摘事項を反映

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,11 +5,46 @@ require 'debug'
 
 options = ARGV.getopts('l', 'w', 'c')
 
-def main
+def main(options)
   files = make_source_files
-  print_outputs(files)
+  print_outputs(files, options)
   return unless files.size > 1
-  print_total(files)
+
+  print_total(files, options)
+end
+
+def print_outputs(files, options)
+  if options.value?(true)
+    count_outputs(files).each do |file|
+      print file[:lines] if options['l']
+      print file[:words] if options['w']
+      print file[:bytes] if options['c']
+      print (file[:name]).to_s
+      print "\n"
+    end
+  else
+    count_outputs(files).each do |file|
+      print file[:lines]
+      print file[:words]
+      print file[:bytes]
+      print (file[:name]).to_s
+      print "\n"
+    end
+  end
+end
+
+def print_total(files, options)
+  totals = count_total(files)
+  if options.value?(true)
+    print totals[:number_of_lines] if options['l']
+    print totals[:number_of_words] if options['w']
+    print totals[:number_of_bytes] if options['c']
+  else
+    totals.each_value do |total|
+      print total.to_s.rjust(8)
+    end
+  end
+  print ' total'
 end
 
 def make_source_files
@@ -35,9 +70,9 @@ end
 def count_outputs(files)
   files.map do |file|
     {
-      lines: count_lines(file[:file_contents]),
-      words: count_words(file[:file_contents]),
-      bytes: count_byte(file[:file_contents]),
+      lines: count_lines(file[:file_contents]).to_s.rjust(8),
+      words: count_words(file[:file_contents]).to_s.rjust(8),
+      bytes: count_byte(file[:file_contents]).to_s.rjust(8),
       name: " #{file[:file_name]}"
     }
   end
@@ -55,29 +90,12 @@ def count_byte(file)
   file.bytesize
 end
 
-def print_outputs(files)
-  options = ARGV.getopts('l', 'w', 'c')
-  count_outputs(files).each do |file|
-    file.each_value do |f|
-      print f.to_s.rjust(8)
-    end
-    print "\n"
-  end
-end
-
 def count_total(files)
   {
-    number_of_lines: files.sum { |file| file[:file_contents].to_s.lines.count },
-    number_of_words: files.sum { |file| file[:file_contents].to_s.split(/\s+/).size },
-    number_of_bytes: files.sum { |file| file[:file_contents].to_s.bytesize }
+    number_of_lines: files.sum { |file| file[:file_contents].to_s.lines.count }.to_s.rjust(8),
+    number_of_words: files.sum { |file| file[:file_contents].to_s.split(/\s+/).size }.to_s.rjust(8),
+    number_of_bytes: files.sum { |file| file[:file_contents].to_s.bytesize }.to_s.rjust(8)
   }
 end
 
-def print_total(files)
-  count_total(files).each_value do |file|
-    print file.to_s.rjust(8)
-  end
-  print ' total'
-end
-
-main
+main(options)
