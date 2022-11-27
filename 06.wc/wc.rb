acdbf82ae7b89470f17ab1cbe1c3520b64b33b3a
umizaru <y.wakita1T@gmail.com>
2022-11-27 23:25:44 +0900

    動画レビュー2回目の指摘を反映

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,7 +5,9 @@ require 'optparse'
 def main
   options = ARGV.getopts('l', 'w', 'c')
   files = make_source_files
-  print_outputs(options, files)
+  outputs = count_outputs(files)
+  outputs_including_total = count_outputs_including_total(outputs)
+  print_outputs(options, outputs_including_total)
 end
 
 def make_source_files
@@ -17,45 +19,10 @@ def make_source_files
       }
     end
   else
-    stdins = $stdin.read.split(',')
-    stdins.map do |stdin|
-      { file_contents: stdin }
-    end
+    stdin = $stdin.read
+    h = []
+    h << { file_contents: stdin }
   end
-end
-
-def print_outputs(options, files)
-  rjust_count_outputs_including_total(files).each do |file|
-    if options.value?(true)
-      print options['l'] == true ? file[:lines] : ''
-      print options['w'] == true ? file[:words] : ''
-      print options['c'] == true ? file[:bytes] : ''
-    else
-      print "#{file[:lines]}#{file[:words]}#{file[:bytes]}"
-    end
-    print(file[:name])
-    print "\n"
-  end
-end
-
-def rjust_count_outputs_including_total(files)
-  count_outputs_including_total(files).each do |file|
-    file.each do |key, value|
-      file[key] = value.to_s.rjust(8) if key != :name
-    end
-  end
-end
-
-def count_outputs_including_total(files)
-  outputs = count_outputs(files)
-  totals =
-    {
-      lines: outputs.sum { |file| file[:lines] },
-      words: outputs.sum { |file| file[:words] },
-      bytes: outputs.sum { |file| file[:bytes] },
-      name: ' total'
-    }
-  outputs.size > 1 ? outputs.push(totals) : outputs
 end
 
 def count_outputs(files)
@@ -64,7 +31,7 @@ def count_outputs(files)
       lines: count_lines(file[:file_contents]),
       words: count_words(file[:file_contents]),
       bytes: count_bytes(file[:file_contents]),
-      name: " #{file[:file_name]}"
+      name: (file[:file_name]).to_s
     }
   end
 end
@@ -79,6 +46,33 @@ end
 
 def count_bytes(file)
   file.bytesize
+end
+
+def count_outputs_including_total(outputs)
+  if outputs.size > 1
+    totals =
+      {
+        lines: outputs.sum { |file| file[:lines] },
+        words: outputs.sum { |file| file[:words] },
+        bytes: outputs.sum { |file| file[:bytes] },
+        name: 'total'
+      }
+    outputs.push(totals)
+  else
+    outputs
+  end
+end
+
+def print_outputs(options, outputs)
+  outputs.each do |file|
+    #   file.each do |key, value|
+    #     file[key] = value.to_s.rjust(8) if key != :name
+    # end
+    print file[:lines] if options['l'] || options.values.all? { |v| v == false }
+    print file[:words] if options['w'] || options.values.all? { |v| v == false }
+    print file[:bytes] if options['c'] || options.values.all? { |v| v == false }
+    puts " #{file[:name]}"
+  end
 end
 
 main
