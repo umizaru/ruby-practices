fdbc1aa41c781ba0ca1744900d5bfae568dd0b7a
umizaru <y.wakita1T@gmail.com>
2023-04-15 22:05:23 +0900

    aオプションの表示を修正

--- 08.ls_object/file_detail_retriever.rb
+++ 08.ls_object/file_detail_retriever.rb
@@ -10,7 +10,7 @@ class FileDetailRetriever
   end
 
   def retrieve
-    files_name = @all ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    files_name = @all ? Dir.entries('.').sort : Dir.glob('*')
     files_name = files_name.reverse if @reverse
     files_name.map { |file_name| FileDetail.new(file_name) }
   end
